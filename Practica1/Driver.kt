704ce17f2d52ea7625c9b021764ea601ca7ae145
bebrium <cumakovaleksej76@gmail.com>
2025-12-14 16:45:18 +0700

    Упрощение кода

--- Practica1/Driver.kt
+++ Practica1/Driver.kt
@@ -1,20 +1,16 @@
 class Driver(
     fullName: String,
     age: Int,
-    override var currentSpeed: Double,
-    var direction: Double = Math.PI / 2
+    currentSpeed: Double
 ) : Human(fullName, age, currentSpeed) {
 
+
     override fun move(dt: Double) {
-        val dx = currentSpeed * Math.cos(direction) * dt
-        val dy = currentSpeed * Math.sin(direction) * dt
-        x += dx
-        y += dy
+        x += currentSpeed * dt
     }
 
     override fun printInfo() {
-        println("$fullName | возраст: $age | скорость: ${"%.2f".format(currentSpeed)} м/с | координаты: (${x.format(2)}, ${y.format(2)}) | направление: ${direction.format(2)} рад")
+        println("$fullName (Водитель) | Возраст: $age | Скорость: ${currentSpeed.format(2)} м/с | " +
+                "Координаты: (${x.format(2)}, ${y.format(2)})")
     }
-
-    private fun Double.format(digits: Int): String = "%.${digits}f".format(this)
 }
